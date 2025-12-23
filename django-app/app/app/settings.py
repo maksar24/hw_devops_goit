b9980b00398d62f18b62f3a00da46b6eb5297b6f
maksar24 <leskovecmaksim@gmail.com>
2025-12-23 15:46:58 +0000

    Add minimal Django app

---
+++ django-app/app/app/settings.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'dev-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = []
+
+ROOT_URLCONF = 'app.urls'
+
+TEMPLATES = []
+
+WSGI_APPLICATION = 'app.wsgi.application'
+
+DATABASES = {}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -61,3 +61,15 @@ resource "aws_nat_gateway" "natgw" {
 
   depends_on = [aws_internet_gateway.igw]
 }
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_assoc" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
